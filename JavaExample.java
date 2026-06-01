1f1d16740596d6755033a35a0c919b6b3c855679
mujawarmdali8-png <mujawarmdali8@gmail.com>
2026-06-01 00:54:46 -0700

    Add files via upload

---
+++ JavaExample.java
@@ -0,0 +1,18 @@
+class JavaExample{
+    public static void main(String [] args)
+    {
+        byte b=100;
+        short s=200;
+        int num=1234321;
+        long L=123456543L;
+        double num1=22.4;
+        float num2=22.4f;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(L);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+
+}
